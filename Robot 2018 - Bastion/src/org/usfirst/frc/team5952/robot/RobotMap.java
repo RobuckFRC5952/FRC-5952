4eacf2b946f4afd41f0af278da9d43c758f2f481
Adrien Corbin <adrien.corbin.92@gmail.com>
2018-02-19 18:31:50 -0500

    edition des ports du robot

--- Robot 2018 - Bastion/src/org/usfirst/frc/team5952/robot/RobotMap.java
+++ Robot 2018 - Bastion/src/org/usfirst/frc/team5952/robot/RobotMap.java
@@ -8,22 +8,22 @@
 package org.usfirst.frc.team5952.robot;
 
 public class RobotMap {
-	public static int liftMotor = 2; // PWM
-	public static int liftEncoder1 = 1;
-	public static int liftEncoder2 = 2;
-	public static int topLiftLimitSwitchChannel = 1;
-	public static int bottomLiftLimitSwitchChannel = 2;
+	public static int liftMotor = 0; // PWM
+	public static int liftEncoder1 = 6; // DIO
+	public static int liftEncoder2 = 7;// DIO 
+	public static int topLiftLimitSwitchChannel = 8; // DIO
+	public static int bottomLiftLimitSwitchChannel = 9; // DIO
 	
-	public static int cableWinchMotor = 2; // PWM
-	public static int cableWinchEncoder1 = 1;
-	public static int cableWinchEncoder2 = 2;
+	public static int cableWinchMotor = 1; // PWM
+	public static int cableWinchEncoder1 = 4; // DIO
+	public static int cableWinchEncoder2 = 5; // DIO
 	
-	public static int driveTrainMotorRight = 0; // PWM
-	public static int driveTrainMotorLeft = 1; // PWM
-	public static int driveTrainLeftEncoder1 = 0; 
-	public static int driveTrainLeftEncoder2 = 1;
-	public static int driveTrainRightEncoder1 = 2;
-	public static int driveTrainRightEncoder2 = 3;
+	public static int driveTrainMotorRight = 3; // PWM
+	public static int driveTrainMotorLeft = 2; // PWM
+	public static int driveTrainLeftEncoder1 = 2; // DIO
+	public static int driveTrainLeftEncoder2 = 3; // DIO
+	public static int driveTrainRightEncoder1 = 0; // DIO
+	public static int driveTrainRightEncoder2 = 1; // DIO
 	
 	public static int clampUpDown = 1;
 	public static int clampOpenClose = 2;
